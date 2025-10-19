8612774bd41d72db60b7e28bae0107927a3f152c
RShaxinya <shaxathrasher@gmail.com>
2025-10-19 14:00:46 +0500

    Create Vector_w.h

---
+++ Vector_w.h
@@ -0,0 +1,14 @@
+#ifndef VECTOR_W
+#degine VECTOR_W
+
+typedef struct{
+double x;
+double y;
+double z;
+}Vector;
+
+Vector add(Vector a,Vector b);
+Vector sub(Vector a,Vector b);
+Vector dot(Vector a,Vector b);
+
+#endif
