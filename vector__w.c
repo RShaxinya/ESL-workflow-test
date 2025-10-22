61f8a73952d7d1bcef5440e7c0e30b9beceec96b
RShaxinya <shaxathrasher@gmail.com>
2025-10-22 12:40:18 +0500

    vector__w.c

--- vector__w.c
+++ vector__w.c
@@ -1,11 +1,17 @@
 #include "vector_w.h"
 
 Vector add(Vector a, Vector b){
-return (Vector)(a.x+b.x,a.y+b.y,a.z+b.z)
+Vector result  = (a.x+b.x,a.y+b.y,a.z+b.z);
+return result ;
 }
 Vector sub(Vector a, Vector b){
-return (Vector)(a.x-b.x,a.y-b.y,a.z-b.z)
+Vector result = (a.x-b.x,a.y-b.y,a.z-b.z)
+return result ;
 }
 double dot(Vector a, Vector b,){
 return a.x*b.x+a.y*b.y+a.z*b.z;
+}
+Vector cross (Vector a, Vector b){ 
+Vector result = {a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x};
+return result;
 }
